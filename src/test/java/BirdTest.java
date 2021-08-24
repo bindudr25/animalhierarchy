d9cd78c5ae2cf3df92bdae078eddf08794c6df37
bindudr25 <bindu.dr25@gmail.com>
2021-08-24 23:11:35 +0800

    cleanup, and added tests

--- src/test/java/BirdTest.java
+++ src/test/java/BirdTest.java
@@ -1,4 +1,6 @@
 import com.animal.Bird;
+import com.animal.Chicken;
+import com.animal.Duck;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,10 +17,37 @@ class BirdTest {
     }
 
     @Test
-    public void testBird() {
+    public void testBirdSing() {
         Bird bird = new Bird();
         bird.sing();
         Assertions.assertEquals("I am singing", outputStreamCaptor.toString()
+                .trim());
+
+    }
+
+    @Test
+    public void testDuckSay() {
+        Duck duck = new Duck();
+        duck.say();
+        Assertions.assertEquals("Quack, quack", outputStreamCaptor.toString()
+                .trim());
+
+    }
+
+    @Test
+    public void testDuckSwim() {
+        Duck duck = new Duck();
+        duck.swim();
+        Assertions.assertEquals("I am Swimming", outputStreamCaptor.toString()
+                .trim());
+
+    }
+
+    @Test
+    public void testChickenSay() {
+        Chicken chicken = new Chicken();
+        chicken.say();
+        Assertions.assertEquals("Cluck, Cluck", outputStreamCaptor.toString()
                 .trim());
 
     }
